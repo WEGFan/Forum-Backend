93892493a8491f71f38391663dc68be7da4d84d6
WEGFan <goofans@qq.com>
2020-08-24 21:41:22 +0800

    add: [CustomOrikaMapperFactoryBuilderConfigurer] 根据项目配置动态启用orika调试

--- src/main/java/cn/wegfan/forum/config/orika/CustomOrikaMapperFactoryBuilderConfigurer.java
+++ src/main/java/cn/wegfan/forum/config/orika/CustomOrikaMapperFactoryBuilderConfigurer.java
@@ -4,11 +4,20 @@ import ma.glasnost.orika.MapperFactory;
 import ma.glasnost.orika.converter.ConverterFactory;
 import ma.glasnost.orika.converter.builtin.BuiltinConverters;
 import ma.glasnost.orika.impl.DefaultMapperFactory;
+import ma.glasnost.orika.impl.generator.EclipseJdtCompilerStrategy;
+import ma.glasnost.orika.impl.generator.JavassistCompilerStrategy;
 import net.rakugakibox.spring.boot.orika.OrikaMapperFactoryBuilderConfigurer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CustomOrikaMapperFactoryBuilderConfigurer implements OrikaMapperFactoryBuilderConfigurer {
+
+    /**
+     * 当前生效的项目配置名称
+     */
+    @Value("${spring.profiles.active}")
+    private String activeProfile;
 
     @Override
     public void configure(DefaultMapperFactory.MapperFactoryBuilder<?, ?> mapperFactoryBuilder) {
@@ -16,6 +25,9 @@ public class CustomOrikaMapperFactoryBuilderConfigurer implements OrikaMapperFac
                 // 先不使用内置转换器，让自定义转换器先注册
                 .useBuiltinConverters(false)
                 .dumpStateOnException(true)
+                .compilerStrategy("dev".equals(activeProfile)
+                        ? new EclipseJdtCompilerStrategy()
+                        : new JavassistCompilerStrategy())
                 .build();
 
         ConverterFactory converterFactory = mapperFactory.getConverterFactory();
