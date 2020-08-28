3bc8c7b3dc20560143bccca1e89c55a53b4b3c75
WEGFan <goofans@qq.com>
2020-08-28 15:18:52 +0800

    add: [PermissionServiceImpl] 获取权限添加缓存

--- src/main/java/cn/wegfan/forum/service/PermissionServiceImpl.java
+++ src/main/java/cn/wegfan/forum/service/PermissionServiceImpl.java
@@ -1,5 +1,6 @@
 package cn.wegfan.forum.service;
 
+import cn.hutool.core.util.StrUtil;
 import cn.wegfan.forum.mapper.PermissionMapper;
 import cn.wegfan.forum.model.entity.Permission;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
@@ -8,6 +9,9 @@ import lombok.extern.slf4j.Slf4j;
 import ma.glasnost.orika.MapperFacade;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -25,22 +29,29 @@ public class PermissionServiceImpl extends ServiceImpl<PermissionMapper, Permiss
     @Autowired
     private MapperFacade mapperFacade;
 
+    @Autowired
+    private RedisTemplate<String, Object> redisTemplate;
+
     @Override
+    @Cacheable(key = "'forum:' + #userId")
     public Permission getForumPermissionByUserId(Long userId) {
         return permissionMapper.selectForumPermissionByUserId(userId);
     }
 
     @Override
+    @Cacheable(key = "'board:' + #boardId")
     public Permission getBoardPermissionByBoardId(Long boardId) {
         return permissionMapper.selectBoardPermissionByBoardId(boardId);
     }
 
     @Override
+    @Cacheable(key = "'userBoard:' + #userId + '_' + #boardId")
     public Permission getUserBoardPermissionByUserIdAndBoardId(Long userId, Long boardId) {
         return permissionMapper.selectUserBoardPermissionByUserIdAndBoardId(userId, boardId);
     }
 
     @Override
+    @CacheEvict(key = "'forum:' + #permission.userId")
     public boolean addOrUpdateForumPermission(Permission permission) {
         return saveOrUpdate(permission, Wrappers.<Permission>lambdaUpdate()
                 .eq(Permission::getUserId, permission.getUserId())
@@ -48,6 +59,7 @@ public class PermissionServiceImpl extends ServiceImpl<PermissionMapper, Permiss
     }
 
     @Override
+    @CacheEvict(key = "'board:' + #permission.boardId")
     public boolean addOrUpdateBoardPermission(Permission permission) {
         return saveOrUpdate(permission, Wrappers.<Permission>lambdaUpdate()
                 .eq(Permission::getBoardId, permission.getBoardId())
@@ -55,6 +67,7 @@ public class PermissionServiceImpl extends ServiceImpl<PermissionMapper, Permiss
     }
 
     @Override
+    @CacheEvict(key = "'userBoard:' + #permission.userId + '_' + #permission.boardId")
     public boolean addOrUpdateUserBoardPermission(Permission permission) {
         return saveOrUpdate(permission, Wrappers.<Permission>lambdaUpdate()
                 .eq(Permission::getUserId, permission.getUserId())
@@ -63,7 +76,12 @@ public class PermissionServiceImpl extends ServiceImpl<PermissionMapper, Permiss
 
     @Override
     public List<Permission> listBoardPermissionsByUserId(Long userId) {
-        return permissionMapper.selectBoardPermissionListByUserId(userId);
+        List<Permission> permissionList = permissionMapper.selectBoardPermissionListByUserId(userId);
+        permissionList.forEach(item -> {
+            String key = StrUtil.format("forum:permission::userBoard:{}_{}", userId, item.getBoardId());
+            redisTemplate.opsForValue().set(key, item);
+        });
+        return permissionList;
     }
 
 }
